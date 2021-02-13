d886b6e6fb4e2d8d383cd7311f3a9921d166d744
dillanmerchant <60758198+dillanmerchant@users.noreply.github.com>
2021-02-13 10:11:34 -0800

    fixed async, not loading data on first load

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,22 @@
 // Script.js
 
-window.addEventListener('DOMContentLoaded', () => {
-  
-  if (localStorage.getItem("storeAPIData") == null) {
+const getPoductList = async () => {
+  const res = await fetch('https://fakestoreapi.com/products');
+  const products = await res.json();
+  localStorage.setItem('storeAPIData', JSON.stringify(products));
+  return products;
+};
+
+window.addEventListener('DOMContentLoaded', async () => {
+
+  //NOT GIVING API DATA ON FIRST RUN
+  /*if (localStorage.getItem('storeAPIData') == null) {
     fetch('https://fakestoreapi.com/products')
     .then(response => response.json())
-    .then(data => localStorage.setItem("storeAPIData", JSON.stringify(data)))
-  }
+    .then(data => localStorage.setItem('storeAPIData', JSON.stringify(data)))
+  }*/
+
+  const storeData = await getPoductList();
 
   let productList = document.getElementById('product-list');
 
@@ -25,7 +35,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   cart = JSON.parse(localStorage.getItem('cart'));
 
-  JSON.parse(localStorage.getItem("storeAPIData")).forEach(function (product) {
+  storeData.forEach(function (product) {
     let el = document.createElement('product-item');
     el = new ProductItem(product.id, product.image, product.title, product.price, cart.includes(product.id));
     productList.appendChild(el);
